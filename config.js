e76b921c7e471472adbbe48dc952d594d5aa5b81
EV <chk.evp@gmail.com>
2025-08-18 15:44:14 -0700

    remove gitignore to post firebase creds

---
+++ config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBLjFbdApHTK3JUhP2HTF0JrvRv9pRQS_M",
+    authDomain: "ev4-portfolio.firebaseapp.com",
+    projectId: "ev4-portfolio",
+    storageBucket: "ev4-portfolio.firebasestorage.app",
+    messagingSenderId: "1014414842654",
+    appId: "1:1014414842654:web:ced79d728f7835fb30c70f",
+    measurementId: "G-GYMXQMWGC5"
+    };
